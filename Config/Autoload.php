1743be382418dc9f16be68710bd19f6f0569b685
nachorios <riosignacio312@gmail.com>
2019-10-04 13:51:50 -0300

    models

--- Config/Autoload.php
+++ Config/Autoload.php
@@ -7,6 +7,7 @@ class Autoload {
           spl_autoload_register(function($classNotFound)
           {
                // Armo la url de la clase a partir del namespace y la instancia.
+<<<<<<< HEAD
                $url = ROOT . str_replace("\\", "/", $classNotFound)  . ".php";
                $url = str_replace("\\","/",$url);
                
@@ -14,6 +15,10 @@ class Autoload {
                //$url = str_replace("Controllers/","/",$url);
                //echo $url . " ". $classNotFound . '<br/> ';
                
+=======
+               $url = ROOT . '/' . str_replace("\\", "/", $classNotFound)  . ".php";
+
+>>>>>>> models
                // Incluyo la url que, si todo esta bien, debería contener la clase que intento instanciar.
                include_once($url);
           });
---
+++ Models/Person.php
@@ -0,0 +1,37 @@
+<?php namespace Models;
+
+class Person{
+    private $name;
+    private $lastName;
+    private $dni;
+
+    public function __construct($name, $lastName, $dni){
+        $this->name = $name;
+        $this->lastName = $lastName;
+        $this->dni = $dni;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function getLastName(){
+        return $this->lastName;
+    }
+
+    public function getDni(){
+        return $this->dni;
+    }
+
+    public function setName($name){
+        $this->name = $name;
+    }
+
+    public function setLastName($lastName){
+        $this->lastName = $lastName;
+    }
+
+    public function setDni($dni){
+        $this->dni = $dni;
+    }
+}
