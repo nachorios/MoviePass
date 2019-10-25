6a0d28250a51fe1e7a7b27fc66341273137deabd
Limancio <luis.monteza@hotmail.com>
2019-10-25 12:18:22 -0300

    correcion de editar cine.

--- Forms/cinema-edit-form.php
+++ Forms/cinema-edit-form.php
@@ -1,21 +1,21 @@
 <form action="<?php echo URL?>/cinema/editCinema" method="POST">
   <div class="form-group">
     <label for="nameCinema"> Nombre del cine: </label>
-    <input type="text" name="name" class="form-control" id="nameCinema" value="<?php if($cinemaEdit != null) echo $cinemaEdit->getName() ?>" required> </input>
+    <input type="text" name="name" class="form-control" id="nameCinema" value="" required> </input>
   </div>
   <div class="form-group">
     <label for="capacityCinema"> Capacidad de sala: </label>
-    <input type="number" min="0" max="200" name="capacity" class="form-control" id="capacityCinema" value="<?php if($cinemaEdit != null) echo $cinemaEdit->getCapacity() ?>" required> </input>
+    <input type="number" min="0" max="200" name="capacity" class="form-control" id="capacityCinema" value="" required> </input>
   </div>
   <div class="form-group">
     <label for="adressCinema"> Direccion: </label>
-    <input type="text" name="adress" class="form-control" id="adressCinema" value="<?php if($cinemaEdit != null) echo $cinemaEdit->getAdress() ?>" required> </input>
+    <input type="text" name="adress" class="form-control" id="adressCinema" value="" required> </input>
   </div>
   <div class="form-group">
-    <label for="valueCine"> Valor entrada: </label>
-    <input type="number" min="0" name="value" class="form-control" id="valueCine" value="<?php if($cinemaEdit != null) echo $cinemaEdit->getValue() ?>" required> </input>
+    <label for="valueCinema"> Valor entrada: </label>
+    <input type="number" min="0" name="value" class="form-control" id="valueCinema" value="" required> </input>
   </div>
-  <input type="text" name="editCinema" class="form-control" id="editCinema" value="<?php if(isset($_GET['edit'])){ echo $_GET['edit']; }?>" hidden> </input>
+  <input type="text" name="editCinema" class="form-control" id="editCinema" value="" hidden> </input>
   
   <div class="text-center">
     <button type="submit" class="btn btn-info btn-lg">Editar</button>
