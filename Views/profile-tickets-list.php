9b36bd2f16484a841a12765de5b6375f6d793b20
ignacio chiaradia <ignachy01@gmail.com>
2019-11-20 00:08:00 -0300

    Boton borrar entrada adquirida en proceso

--- Views/profile-tickets-list.php
+++ Views/profile-tickets-list.php
@@ -24,6 +24,7 @@
             <th>Hora</th>
             <th>Sala</th>
             <!--<th>Codigo QR</th>-->
+            <th>Eliminar</th>
         </thead>
         <tbody>
         <?php foreach($userTickets as $ticket):?>
@@ -34,6 +35,12 @@
                 <td><?php echo $ticket->getFunction()->getHour(); ?></td>
                 <td><?php echo $ticket->getFunction()->getSaloon()->getName(); ?></td>
                 <!--<td>QR</td>-->
+                <td><a href="?delete=<?php /*echo $ticket->getIdBill()*/ ?>" class="btn btn-danger"><i class="fa fa-trash-o"></i></a></td>
+<!--                <button type="button" onclick="loading(this, 'danger', ''); window.location='<?php //echo URL ?>/Cinema/deleteCinema?delete=<?php //echo $cinema->getIdCinema()  ?>'" class="btn btn-danger"><i class="fa fa-trash-o"> Eliminar</i></button>
+
+                <a href="?delete=<?php //echo $value->getEmail() ?>" class="btn btn-light">
+                    <object type="image/svg+xml" data="icons/trash-2.svg" width="16" height="16"></object>
+                </a>-->
             </tr>
         <?php endforeach; ?>
         </tbody>
